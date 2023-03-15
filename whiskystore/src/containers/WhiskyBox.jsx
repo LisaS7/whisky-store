e5268f02c598e3deed5e978fb2d355d7bb53a6e5
Lisa <mclisa00@gmail.com>
2023-03-15 14:15:41 +0000

    Basket context

--- whiskystore/src/containers/WhiskyBox.jsx
+++ whiskystore/src/containers/WhiskyBox.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import WhiskyList from "../components/Whisky/WhiskyList";
 import products from "../data/products.json";
 
 export default function WhiskyBox() {
-  const [user, setUser] = useState({});
-
   return (
     <main>
       <WhiskyList products={products} />
